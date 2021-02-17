bb72bde44c0a53f12cd0c226b7ebe08bda2bfe26
Niccolo Delfino <delfino.nicco@libero.it>
2021-02-17 11:46:19 +0100

    Build ok

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 var app = new Vue({
   el: `#app`,
   data: {
-    loghi: ['amazon', 'google', 'lenovo', 'paypal', 'shopify', 'spotify']
+    loghi: ['amazon', 'google', 'lenovo', 'paypal', 'shopify', 'spotify'],
+    tools:['Digital Marketing Solutions for Tomorrow', 'Our Talented & Experienced Marketing Agency', 'Create your own skin to match your brand'],
   },
   mounted(){
 
