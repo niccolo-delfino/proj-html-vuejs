865f159aba7a2f9f68b21c40c99021e0732bbdd6
Niccolo Delfino <delfino.nicco@libero.it>
2021-02-17 17:17:37 +0100

    slide working

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 var app = new Vue({
   el: `#app`,
   data: {
+    counter: 0,
+    span:['prova1', 'prova2', 'prova3', 'prova4'],
     loghi: ['amazon', 'google', 'lenovo', 'paypal', 'shopify', 'spotify'],
+    quick: ['npm', 'nuget', 'spm', 'github'],
     tools:['Digital Marketing Solutions for Tomorrow', 'Our Talented & Experienced Marketing Agency', 'Create your own skin to match your brand'],
     rates:['Full Access', 'Enhanced Security', 'Source Files', '1 Domain Free','Enhanced Security'],
     company:['About us', 'Services', 'Team', 'Pricing', 'Project', 'Careers', 'Blog', 'Login'],
@@ -12,6 +15,9 @@ var app = new Vue({
 
   },
   methods: {
-
+    rettangolo(indice){
+      this.counter = indice;
+      console.log(this.counter);
+    }
   }
 });
