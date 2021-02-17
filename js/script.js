d47aed6bd368884cdb1bfe16d52a0fcab6b7736f
Niccolo Delfino <delfino.nicco@libero.it>
2021-02-17 14:51:43 +0100

    rates ok

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var app = new Vue({
   data: {
     loghi: ['amazon', 'google', 'lenovo', 'paypal', 'shopify', 'spotify'],
     tools:['Digital Marketing Solutions for Tomorrow', 'Our Talented & Experienced Marketing Agency', 'Create your own skin to match your brand'],
+    rates:['Full Access', 'Enhanced Security', 'Source Files', '1 Domain Free','Enhanced Security'],
   },
   mounted(){
 
