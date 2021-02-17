5287c06a85a24bbe2b4b76d4e71a1ec925be30d7
Niccolo Delfino <delfino.nicco@libero.it>
2021-02-17 16:49:57 +0100

    WIP footer

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ var app = new Vue({
     loghi: ['amazon', 'google', 'lenovo', 'paypal', 'shopify', 'spotify'],
     tools:['Digital Marketing Solutions for Tomorrow', 'Our Talented & Experienced Marketing Agency', 'Create your own skin to match your brand'],
     rates:['Full Access', 'Enhanced Security', 'Source Files', '1 Domain Free','Enhanced Security'],
+    company:['About us', 'Services', 'Team', 'Pricing', 'Project', 'Careers', 'Blog', 'Login'],
+    links: ['Terms of Services', 'Privacy Policy', 'Documentation', 'Changelog', 'Component']
+
   },
   mounted(){
 
