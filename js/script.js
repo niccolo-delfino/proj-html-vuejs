5060310459ea26aa2fbb098801c65de068c5fe72
Niccolo Delfino <delfino.nicco@libero.it>
2021-02-18 15:17:21 +0100

    hover missing

--- js/script.js
+++ js/script.js
@@ -9,8 +9,8 @@ var app = new Vue({
     tools:['Digital Marketing Solutions for Tomorrow', 'Our Talented & Experienced Marketing Agency', 'Create your own skin to match your brand'],
     rates:['Full Access', 'Enhanced Security', 'Source Files', '1 Domain Free','Enhanced Security'],
     company:['About us', 'Services', 'Team', 'Pricing', 'Project', 'Careers', 'Blog', 'Login'],
-    links: ['Terms of Services', 'Privacy Policy', 'Documentation', 'Changelog', 'Component']
-
+    links: ['Terms of Services', 'Privacy Policy', 'Documentation', 'Changelog', 'Component'],
+    payment:['american-ex', 'discover', 'master-card', 'paypal', 'visa']
   },
   mounted(){
 
